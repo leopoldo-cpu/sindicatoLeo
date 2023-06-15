dceac86a2ffd20ddfa5f3f0448153382b87aec43
Rigau Leopoldo <l.rigau@etec.local>
2023-06-15 10:32:31 -0300

    conexion

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const {ObjectId} = require('mongodb')
 const mongoose = require('mongoose')
 const MONGO_HOST="localhost"
 const MONGO_PUERTO="27017"
-const MONGO_URI="mongodb://root:example@192.168.44.122:27017"
+const MONGO_URI="mongodb://root:example@192.168.44.122:27017/registro?authSource=admin"
+
 
 app.use(
     express.urlencoded({
@@ -18,22 +19,35 @@ app.use(
         type:"*/*"
     })
 )
+
+
+
+
+
 app.use(cors())
 let vari = {
     "Nombre":"funcional",
     "Gmal":"funcional",
     "Contraseña":"funcional"
-
-
 }
-const client = new MongoClient(MONGO_URI) //me conecto con URI
+mongoose.connect(MONGO_URI)//me conecto con URI
 const dbname = "registro"
 const coll = "usuarios"
-let coleccion = client.db(dbname).collection(coll) //creo el db.coll
+
+const esquemaUsuarios = new mongoose.Schema({
+    Nombre: String,
+    Gmal: String,
+    Contraseña: String,
+    prueba: String
+})
+
+const modeloUsuario = mongoose.model(coll, esquemaUsuarios)
 
 
-
-
+app.get(("/"), async(rec,res)=>{
+    let usuarios = await modeloUsuario.find()
+    res.send({usuarios})
+})
 
 
 
@@ -41,7 +55,7 @@ async function main(){
     let insertar = await  coleccion.insertOne(vari) //inserto un documento
     console.log(insertar);
 }
-main()
+//main()
 app.listen(3120,(port = 3120)=>{
     console.log("Hola mundo")
     console.log("Servidor corriendo en 127.0.0.1:"+port);
